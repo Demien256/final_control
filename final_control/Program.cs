2b8f299ab2241cac8a62dec91c8fb1f27877f087
Demien_Hall <>
2024-06-26 00:47:06 +0300

    массив newArray

--- final_control/Program.cs
+++ final_control/Program.cs
@@ -35,3 +35,15 @@ for (int i = 0; i <= array.Length - 1; i++)
 {
     if (array[i].Length <= 3) lenNewArray++;
 }
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        idx++;
+    }
+}
