e3e19fd04119d9a5bbfd9e013b9eb55910a63810
Demien_Hall <>
2024-06-26 00:48:19 +0300

    команды для работы с программой

--- final_control/Program.cs
+++ final_control/Program.cs
@@ -47,3 +47,19 @@ for (int i = 0; i <= array.Length - 1; i++)
         idx++;
     }
 }
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
+
+
+// Функция: Вывод команд для работы с программой
+void Commands()
+{
+    Console.WriteLine();
+    Console.WriteLine("СПИСОК КОМАНД:");
+    Console.WriteLine("1 – использовать массив: [“Hello”, “2”, “world”, “:-)”]");
+    Console.WriteLine("2 – использовать массив: [“1234”, “1567”, “-2”, “computer science”]");
+    Console.WriteLine("3 – использовать массив: [“Russia”, “Denmark”, “Kazan”]");
+    Console.WriteLine();
+}
